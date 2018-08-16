d54ddb8a8465b185c89888d7e6ea538b94256e71
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 01:44:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function createFareMultiplier(i) {
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
-function selectingDrivers(drivers, myFunc) {
+function selectDifferentDrivers(drivers, myFunc) {
   return myfunc(drivers);
 }
